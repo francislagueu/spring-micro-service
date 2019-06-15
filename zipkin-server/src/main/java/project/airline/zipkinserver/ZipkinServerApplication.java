268e4bde83a1fcec7b21c7d5e954c258d8f7e32d
Francis Lagueu <lagson6@yahoo.fr>
2019-06-15 15:43:48 -0700

    Zipkin server configuration

---
+++ zipkin-server/src/main/java/project/airline/zipkinserver/ZipkinServerApplication.java
@@ -0,0 +1,18 @@
+package project.airline.zipkinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+
+@SpringBootApplication
+@EnableZipkinStreamServer
+@EnableEurekaClient
+public class ZipkinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinServerApplication.class, args);
+	}
+
+}
